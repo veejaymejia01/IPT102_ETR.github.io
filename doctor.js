7ad15e90b77e5185b0b5c5d78967618f191cbd62
veejaymejia01 <veejaymejia01@gmail.com>
2026-04-13 11:53:56 +0800

    Update doctor.js

--- doctor.js
+++ doctor.js
@@ -19,7 +19,7 @@ function showSection(id) {
 function logout() {
   localStorage.removeItem('healthcare_token');
   localStorage.removeItem('healthcare_user');
-  window.location.href = 'login.html';
+  window.location.href = 'index.html';
 }
 
 async function apiFetch(url, options = {}) {
