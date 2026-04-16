da703d84bfc3921b91a0f563ad00bf42018c6eae
veejaymejia01 <veejaymejia01@gmail.com>
2026-04-16 23:32:55 +0800

    Update doctor.js

--- doctor.js
+++ doctor.js
@@ -1,6 +1,5 @@
 const API = 'https://etr-backend.onrender.com/api';
 
-
 const token = localStorage.getItem('healthcare_token') || '';
 const currentUser = JSON.parse(localStorage.getItem('healthcare_user') || 'null');
 
