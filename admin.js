0fc081aaeebbd79ed98842feb7ff2a7b443294e6
veejaymejia01 <veejaymejia01@gmail.com>
2026-04-22 15:57:47 +0800

    Update admin.js

--- admin.js
+++ admin.js
@@ -6,7 +6,6 @@ let selectedPatientId = null;
 let adminCalendar = null;
 let selectedCalendarDate = new Date().toISOString().split('T')[0];
 
-
 const currentUser = JSON.parse(localStorage.getItem('healthcare_user') || 'null');
 
 if (!currentUser || currentUser.role !== 'admin') {
