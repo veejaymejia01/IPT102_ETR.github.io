1251da39ecc2e77c1af87ae953c7d5b10185249d
veejaymejia01 <veejaymejia01@gmail.com>
2026-04-22 15:24:03 +0800

    Update doctor.js

--- doctor.js
+++ doctor.js
@@ -5,6 +5,7 @@ let patients = JSON.parse(localStorage.getItem('patients') || '[]');
 let appointments = JSON.parse(localStorage.getItem('appointments') || '[]');
 let doctorCalendar = null;
 
+
 if (!currentUser || currentUser.role !== 'doctor') {
   window.location.href = 'index.html';
 }
